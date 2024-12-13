a4e2ae2bbfb94f9eba866ff4038e906e3e59f0b2
Arkya Bhatt <arkya1509@gmail.com>
2024-12-13 13:01:40 +0530

    update commit

--- Full_Stack_Project/Full_Stack_Project.py
+++ Full_Stack_Project/Full_Stack_Project.py
@@ -4,6 +4,8 @@ import reflex as rx
 
 from rxconfig import config
 
+from .ui.base import base_page
+
 class State(rx.State):
     """The app state."""
     label = "Welcome to Reflex!"
@@ -14,111 +16,9 @@ class State(rx.State):
     def did_click(self):
         print("Hello World did click")
 
-def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(
-        rx.text(text, size="4", weight="medium"), href=url
-    )
-
-
-def navbar() -> rx.Component:
-    return rx.box(
-        rx.desktop_only(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="\logo.jpg",
-                        width="2.25em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="7", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.hstack(
-                    navbar_link("Home", "/#"),
-                    navbar_link("About", "/#"),
-                    navbar_link("Pricing", "/#"),
-                    navbar_link("Contact", "/#"),
-                    spacing="5",
-                ),
-                rx.hstack(
-                    rx.button(
-                        "Sign Up",
-                        size="3",
-                        variant="outline",
-                    ),
-                    rx.button("Log In", size="3"),
-                    spacing="4",
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        rx.mobile_and_tablet(
-            rx.hstack(
-                rx.hstack(
-                    rx.image(
-                        src="\logo.jpg",
-                        width="2em",
-                        height="auto",
-                        border_radius="25%",
-                    ),
-                    rx.heading(
-                        "Reflex", size="6", weight="bold"
-                    ),
-                    align_items="center",
-                ),
-                rx.menu.root(
-                    rx.menu.trigger(
-                        rx.icon("menu", size=30)
-                    ),
-                    rx.menu.content(
-                        rx.menu.item("Home"),
-                        rx.menu.item("About"),
-                        rx.menu.item("Pricing"),
-                        rx.menu.item("Contact"),
-                        rx.menu.separator(),
-                        rx.menu.item("Log in"),
-                        rx.menu.item("Sign up"),
-                    ),
-                    justify="end",
-                ),
-                justify="between",
-                align_items="center",
-            ),
-        ),
-        bg=rx.color("accent", 3),
-        padding="1em",
-        # position="fixed",
-        # top="0px",
-        # z_index="5",
-        width="100%",
-    )
-
-def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
-    # print([type(x) for x in args])
-    if not isinstance(child, rx.Component):
-        child = rx.heading("This is not a valid child element")
-    if hide_navbar:
-        return rx.container(
-        child,
-        rx.logo(),
-        rx.color_mode.button(position="bottom-left"),
-    )
-    return rx.container(
-        navbar(),
-        child,
-        rx.logo(),
-        rx.color_mode.button(position="bottom-left"),
-    )
-
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return base_page(
-        rx.vstack(
+    my_child = rx.vstack(
             rx.heading(State.label, size="9"),
             rx.text(
                 "Get started by editing ",
@@ -138,8 +38,9 @@ def index() -> rx.Component:
             spacing="5",
             justify="center",
             min_height="85vh",
-        ),
-    )
+            id="my-child",
+        )
+    return base_page(my_child)
 
 app = rx.App()
 app.add_page(index)
