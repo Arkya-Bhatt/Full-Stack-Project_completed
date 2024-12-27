40ca78f1418a2a8a9b5eef947d271b2f090c1f1b
Arkya Bhatt <arkya1509@gmail.com>
2024-12-27 15:51:19 +0530

    updated commit

--- Full_Stack_Project/pages/contact.py
+++ Full_Stack_Project/pages/contact.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import reflex as rx
 
 from .. import navigation
@@ -7,15 +9,40 @@ from ..ui.base import base_page
 class ContactState(rx.State):
     form_data: dict = {}
     did_submit: bool = False
+    timeleft: int = 5
+    
+    @rx.var
+    def timeleft_label(self):
+        if self.timeleft < 1:
+            return "No time left" 
+        return f"Time left: {self.timeleft} seconds"
+    
+    @rx.var
+    def thank_you(self):
+        first_name = self.form_data.get("first_name") or ""
+        return f"Thank you {first_name}".strip() + "!"
     
     # @rx.event
-    def handle_submit(self, form_data: dict):
+    async def handle_submit(self, form_data: dict):
         # """Handle the form submit."""
         print(form_data)
         self.form_data = form_data
         self.did_submit = True
+        yield
+        await asyncio.sleep(2)
+        self.did_submit = False
+        yield
+    
+    async def start_timer(self):
+        while self.timeleft > 0:
+            await asyncio.sleep(1)
+            self.timeleft -= 1
+            yield
 
-@rx.page(route=navigation.routes.CONTACT_US_ROUTE)
+@rx.page(
+    on_load=ContactState.start_timer,
+    route=navigation.routes.CONTACT_US_ROUTE,
+)
 
 def contact_page() -> rx.Component:
     my_form = rx.form(
@@ -24,7 +51,7 @@ def contact_page() -> rx.Component:
                         rx.input(
                             name="first_name",
                             placeholder="First Name",
-                            required=True,
+                            required=False,
                             type="text",
                             width="100%",
                         ),
@@ -60,7 +87,8 @@ def contact_page() -> rx.Component:
     )
     my_child = rx.vstack(
             rx.heading("Contact", size="9"),
-            rx.cond(ContactState.did_submit, ContactState.form_data.to_string(), ""),
+            rx.text(ContactState.timeleft_label),
+            rx.cond(ContactState.did_submit, ContactState.thank_you, ""),
             rx.desktop_only(
                 rx.box(
                     my_form,
