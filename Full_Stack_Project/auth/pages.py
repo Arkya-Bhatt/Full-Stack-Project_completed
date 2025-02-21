0416bada3e372f82844eead64e87955a392308b4
Arkya Bhatt <arkya1509@gmail.com>
2025-02-21 23:47:20 +0530

    new commit

--- Full_Stack_Project/auth/pages.py
+++ Full_Stack_Project/auth/pages.py
@@ -1,17 +1,24 @@
 import reflex as rx
 
-from reflex_local_auth.pages import login_page, register_page
+from reflex_local_auth.pages import LoginState, login_form, PADDING_TOP
 
 from ..ui.base import base_page
 
 def my_login_page() -> rx.Component:
     
     return base_page(
-        login_page()
+        rx.center(
+        rx.cond(
+            LoginState.is_hydrated,  # type: ignore
+            rx.card(login_form()),
+        ),
+        padding_top=PADDING_TOP,
+    ),
+        min_height="85vh",
     )
 
-def my_register_page() -> rx.Component:
+# def my_register_page() -> rx.Component:
     
-    return base_page(
-        register_page()
-    )
+#     return base_page(
+#         register_page()
+#     )
